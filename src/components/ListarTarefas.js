e80e11b71e9ad25ef498c06560ee3d53cc3b9d8b
Patrick <patrickpqdt87289@gmail.com>
2021-06-08 19:43:27 -0300

    List Tasks

--- src/components/ListarTarefas.js
+++ src/components/ListarTarefas.js
@@ -1,13 +1,52 @@
+import { useEffect, useState } from 'react';
 
 import { A } from 'hookrouter';
 
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import ItensListTasks from './itensTarefas';
+
 export default function ListarTarefas() {
-    return(
-        <A 
-            href="/cadastrar"
-            className="btn btn-success btn-sm">
-                Nova Tarefa
-        </A>
+
+    const [tasks, setTasks] = useState([]);
+    const [loadTask, setLoadTask] = useState(true);
+
+    useEffect(() => {
+        function getTasks() {
+            const tasksDb = localStorage['tasks'];
+            let listTasks = tasksDb ? JSON.parse(tasksDb) : []; //convert object with parse
+            setTasks(listTasks);
+            console.log(listTasks);
+        }
         
+        if (loadTask) {
+            getTasks();
+            setLoadTask(false);
+        }
+    
+    }, [loadTask]);
+
+    return(
+        <div className="text-center">
+            <h3>Tarefas a fazer</h3>
+            <Table striped bordered hover responsive>
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                        <A 
+                            href="/cadastrar"
+                            className="btn btn-success btn-sm">
+                            <FontAwesomeIcon icon={faPlus}/>&nbsp;Nova Tarefa
+                        </A>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <ItensListTasks tasks={tasks}/>
+                </tbody>
+            </Table>
+        </div>
     );
 }
